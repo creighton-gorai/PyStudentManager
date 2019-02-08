41e4fab2354a7f5cfbe42fd11b872d66e6c00f89
Creighton Gorai <CGorai@hawaii.edu>
2019-02-08 09:17:16 -1000

    Adding in input and requesting

--- functions.py
+++ functions.py
@@ -1,19 +1,31 @@
 students = []
 
-def get_students_titlecase():
-    students_titlecase = []
-    for student in students:
-        students_titlecase = student.title()
-    return students_titlecase
 
-def print_students_titlecase():
-    students_titlecase = get_students_titlecase()
-    print(students_titlecase)
+def get_students():
+    def get_students_titlecase():
+        students_titlecase = []
+        for student in students:
+            students_titlecase.append(student.title())
+        return students_titlecase
+    students_titlecase_names = get_students_titlecase()
+    print(students_titlecase_names)
 
-def add_student(name, student_id=322):
-    student = {"name": name, "student_id": student_id }
-    students.append(name)
 
-student_list = get_students_titlecase()
+def add_student(name, student_id=0000):
+    student = {"name": name, "student_id": student_id}
+    students.append(student)
 
-add_student("Khain", 123)
+
+insert_student = input("Would you like to enter a student to the Databse? (y for yes and n for no)\n")
+
+
+while insert_student == 'y':
+
+    student_list = get_students_titlecase()
+    student_name = input("Enter student name: ")
+    student_id = input("Enter student ID: ")
+
+    add_student(student_name, student_id)
+    insert_student= input("Would you like to add another student? (y for yes and n for no)\n")
+
+print_students_titlecase()
